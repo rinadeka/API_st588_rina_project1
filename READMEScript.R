951f6c31f0b113a4ddae877ce6f3a72bf83e2f8f
Rina Deka <rdeka@ncsu.edu>
2023-06-25 17:27:25 -0400

    Attempt to create read me from local repo #2

--- READMEScript.R
+++ READMEScript.R
@@ -3,7 +3,7 @@
 # purpose: To render foodVignette.Rmd as a markdown (.md) file named README.md for project repo.
 
 rmarkdown::render(
-  input="foodVignette.Rmd",
+  input="C:/Users/rsdek/Documents/repos/food-api-vignette/foodVignette.Rmd",
   output_format = "github_document",
   output_file = "README.md",
   runtime = "static",
@@ -12,8 +12,7 @@ rmarkdown::render(
   knit_meta = NULL,
   envir = parent.frame(),
   run_pandoc = TRUE,
-  quiet = TRUE,
+  quiet = FALSE,
   encoding = "UTF-8"
 )
-
 
