4752c4810f2621dcf376b0bc0cb234553e694e8e
Rina Deka <rdeka@ncsu.edu>
2023-06-25 13:05:10 -0400

    Adding a script to render notebook as README file, adding foodVignette markdown document

---
+++ READMEScript.R
@@ -0,0 +1,17 @@
+# author: Rina Deka
+# date: 06/25/2023
+# purpose: To render foodVignette.Rmd as a markdown (.md) file named README.md for project repo.
+
+rmarkdown::render(
+  input="foodVignette.Rmd",
+  output_format = "github_document",
+  output_file = "README.md",
+  runtime = "static",
+  clean = TRUE,
+  params = NULL,
+  knit_meta = NULL,
+  envir = parent.frame(),
+  run_pandoc = TRUE,
+  quiet = FALSE,
+  encoding = "UTF-8"
+)
